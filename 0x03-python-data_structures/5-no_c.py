85e579b989306456efaaad208644900ba23d32a4
Martian66 <kimmartin049@gmail.com>
2022-12-03 05:31:44 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new = ""
+    new = ''
     for i in my_string:
         if i is not 'c' and i is not 'C':
             new += i
-    return new
+    return (new)
