66a2f198ce081859ef01869d840f4a3312b81d79
Martian66 <kimmartin049@gmail.com>
2022-12-07 06:38:38 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    return max(a_dictionary.iterkeys(), key = lambda k: a_dictionary[k])
+    return max(a_dictionary.iterkeys(), key = lambda k: k[0])
