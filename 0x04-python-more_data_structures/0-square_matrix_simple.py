fa2f6662180cc41be0fba640ad5bcf6f935405ec
Martian66 <kimmartin049@gmail.com>
2022-12-07 02:59:37 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        return [list(map(lamda j: j**2, i)) for i in matrix]
+    return [list(map(lamda j: j**2, i)) for i in matrix]
