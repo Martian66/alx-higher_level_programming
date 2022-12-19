63d61df2ff74c80aa5234925ac376eafbd72cd87
Martian66 <kimmartin049@gmail.com>
2022-12-19 03:27:54 -0800

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        i = a / b
+    except ZeroDivisionError:
+        i = None
+        return
+    finally:
+        print("Inside result: {}"format(i))
+        return i
