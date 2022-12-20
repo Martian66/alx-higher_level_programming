585059a30ae02ecf45046ff1f5ec6575de83d58f
Martian66 <kimmartin049@gmail.com>
2022-12-20 09:03:13 -0800

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Define a class square"""
+"""Define a class Square"""
 
 
 class Square:
     """Represents a square"""
 
-    def __init__(self, size=0)
+    def __init__(self, size=0):
         """Initialize a new square
 
         Args:
-            size (int): the size of the new square
+            size (int): The size of the new square
             """
             self.size = size
 
