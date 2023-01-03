09cd12e9a488ed83e3a358157c2c217657212cc3
Martian66 <kimmartin049@gmail.com>
2023-01-03 07:57:03 -0800

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -20,3 +20,12 @@ class Rectangle:
     def width(self):
         """retrieves width attribute"""
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
