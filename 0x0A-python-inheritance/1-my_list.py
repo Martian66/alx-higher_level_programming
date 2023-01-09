5a0ec4712c5f822a364f65688d8a445ccbaf88ee
Martian66 <kimmartin049@gmail.com>
2023-01-09 02:48:49 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+my base class
+"""
+class Mylist():
+
+    def print_sort(self):
+        """
+        sorts values in ascending order
+        """
+        sorted(self)
+
+        return self
