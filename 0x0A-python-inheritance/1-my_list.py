16e7a5e36d99ddf1b91249663679460a8d590132
Kim Martin <110632901+Martian66@users.noreply.github.com>
2023-01-09 15:45:27 +0200

    Delete 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module for my list"""
-
-
-class MyList(list):
-    """A class that inherits from list"""
-    def print_sort(self):
-        """prints a sorted list"""
-        print(sorted(self))
