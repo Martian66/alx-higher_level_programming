b30999115da230f44ef5d2b9e87e057c4e8c002b
Martian66 <kimmartin049@gmail.com>
2023-01-10 09:08:26 -0800

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,15 +5,12 @@
 class Student:
     """Student class"""
     def __init__(self, first_name, last_name, age):
-        self.first_name = fist_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         if attrs is None:
             return self.__dict__
-        new_dictionary = {}
-        for key, value in self.__dict__.item():
-            if key in attrs:
-                new_dictionary[key] = value
-        return new_dictionary
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
