9172cd69e2f1a6b3fe30ed7785cd11ab3dc356e3
Martian66 <kimmartin049@gmail.com>
2023-01-10 10:55:36 -0800

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,18 +6,13 @@ def pascal_triangle(n):
     """Represents Pascals Triangle of size n"""
     if n <= 0:
         return []
-    pascal = [[] for i in range(n)]
-    i = 0
-    while i < n:
-        if i == 0:
-            pascal[0] = [1]
-        elif i == 1:
-            pascal = [1, 1]
-        else:
-            pascal[i].append(1)
-            for x in range(len(pascal[i - 1]) - 1):
-                a = pascal[i - 1][x:x + 2]
-                pascal[i].append(sum(a))
-            pascal[i].append(1)
-        i += 1
-    return pascal
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
