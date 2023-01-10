520eb45bb1ec8118ad90fa2543c4029935156441
Martian66 <kimmartin049@gmail.com>
2023-01-10 02:19:52 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,6 @@
 
 def read_file(filename=""):
     """File opened in text mode allowing for accessibility"""
-    f = open('filename', 'r', enconding="utf-8")
-    """File being read and then closed upon success"""
-    with open('filename', enconding='utf=8') as f:
-        read_file = f.read()
-
-        f.closed
-
-        True
+    with open(filename, enconding="utf=8") as f:
+        for line in f:
+            print(line, end="")
