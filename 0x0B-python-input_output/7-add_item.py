3ea857d2a6cae2cc1836e45246f8a297b0a17264
Martian66 <kimmartin049@gmail.com>
2023-01-10 08:23:09 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """A script that adds all arguments to a python list and save them to a file"""
 
-import json
-# a python module
+
 import sys
 # used to access system-specific parameters and functions
-from sys import argv
-""" Importing a list in python that contains all the command-line
-arguments passed to the script"""
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-# importing and saving text file to json file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-# importing and loading text file to json file
 
 if __name__ == "__main__":
-    filename = 'add_item.json'
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 try:
     json_list = load_from_json_file("add_item.json")
