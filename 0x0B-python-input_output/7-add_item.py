c86f5608d1f1a8ce9be75efc24b3da21042d93be
Martian66 <kimmartin049@gmail.com>
2023-01-10 06:51:27 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -16,7 +16,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 # importing and loading text file to json file
 
-filename = "add_items.json"
+filename = "add_item.json"
 json_list = []
 
 if os.path.exists(filename):
