b18ae5bc93f302b3e0bdec710e94af9e6e81d32f
Martian66 <kimmartin049@gmail.com>
2023-02-14 09:48:24 -0800

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that list all records of the table
+-- Record should display both the score and name in descending order
+
+SELECT score, name FROM second_table ORDER BY score DESC, name;
