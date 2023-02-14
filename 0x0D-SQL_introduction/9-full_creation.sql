dcd2b6caee71e7069e609dc322d4396998bea614
Martian66 <kimmartin049@gmail.com>
2023-02-14 09:42:35 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- A script that creates a second table with multiple rows
-CREATE TABLE IF NOT EXISTS second_table(id, INT,
+CREATE TABLE IF NOT EXISTS second_table(id INT,
 name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('1', "John", '10');
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('2', "Alex", '3');
