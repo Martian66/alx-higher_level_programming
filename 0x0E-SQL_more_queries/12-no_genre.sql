b69de714850b408d6a703c49f117223ff3f08cbc
Martian66 <kimmartin049@gmail.com>
2023-02-15 09:42:17 -0800

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
 -- A script that lists all shows contained in hbtn_0d_tvshows
-SELECT title, tv_show_genres.genre.id FROM tv_shows
+SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NULL ORDER BY title, tv_show_genres.genre_id;
