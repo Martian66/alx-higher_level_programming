7fd05b8e33d37422f71203355f55747839943836
Martian66 <kimmartin049@gmail.com>
2023-03-13 12:02:14 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -7,6 +7,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let x = 0; x < parseInt(process.argv[2]); x++) {
-      console.log(myString);
-   }
+    console.log(myString);
+  }
 }
