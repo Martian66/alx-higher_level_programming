beed1f4abcd692a3d03b07547885ca445850675d
Martian66 <kimmartin049@gmail.com>
2023-03-13 11:32:48 -0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// A a script that prints x times “C is fun”
+const arg = process.argv[2];
+const x = parseInt(arg);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+}  else {
+   let count = 0;
+   while (count < x) {
+     console.log('C is fun');
+     count++;
+   }
+}
+
