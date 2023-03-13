1905d303f08420f3e8a9b8f034f62721d814daae
Martian66 <kimmartin049@gmail.com>
2023-03-13 06:19:51 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // A script that prints the first argument passed to it
 
 if (process.argv[2] === undefined) {
-	console.log(process.argv[2]);
-}	else {
 	console.log('No argument');
+}	else {
+	console.log(process.argv[2]);
 }
