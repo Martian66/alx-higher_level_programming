c87eec1dc365522cfb7dee54f188b2d489365c03
Martian66 <kimmartin049@gmail.com>
2023-03-14 07:02:43 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-
+// Write a class Rectangle that defines a rectangle
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (Number.isFinite(w) && Number.isFinite(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    } else {
-      console.log('Rectangle dimensions not set');
+   print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 };
