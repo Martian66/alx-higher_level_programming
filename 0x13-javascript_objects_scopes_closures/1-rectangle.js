05cb2a881c1b78745f8e301e0cbe2cb1c26b6de0
Martian66 <kimmartin049@gmail.com>
2023-03-14 05:52:47 -0700

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,7 @@
 // Write a class Rectangle that defines a rectangle
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (Number.isFinite(w) && Number.isFinite(h) && w > 0 && h > 0) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
