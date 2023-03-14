193b044073268746e4a0df417450c7853fb884a4
Martian66 <kimmartin049@gmail.com>
2023-03-14 08:14:10 -0700

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // Write a function that returns the reversed version of a list
 exports.esrever = function (list) {
-  let reversedList = [];
-  while (list.length) {
-    reversedList.push(list.pop());
+  const reversedList = [];
+
+  for (let i = (list.length - 1); i >= 0; i--) {
+    reversedList.push(list[i]);
   }
   return reversedList;
 };
