5f3cecf3fb5aeb5f06a80cb8a9d604f63ef64791
Martian66 <kimmartin049@gmail.com>
2023-03-14 06:00:57 -0700

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
