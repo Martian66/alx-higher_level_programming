663451c845165300670a39c17f4b6069d9e254c7
Martian66 <kimmartin049@gmail.com>
2023-03-14 07:18:08 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,15 +4,15 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
       return;
-     }
-      this.width = w;
-      this.height = h;
-   }
+    }
+    this.width = w;
+    this.height = h;
+  }
 
-   print (c) {
-     c = 'X';
-     for (let i = 0; i < this.height; i++) {
-	console.log(c.repear(this.width));
-     }
-   }
+  print (c) {
+    c = 'X';
+    for (let i = 0; i < this.height; i+) {
+      console.log(c.repeat(this.width));
+    }
+  }
 };
