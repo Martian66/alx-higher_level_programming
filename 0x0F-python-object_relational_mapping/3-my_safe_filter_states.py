d40ef0e2d25f230674eba4901a84b548344ad74e
Martian66 <kimmartin049@gmail.com>
2023-03-17 09:44:55 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" A script that lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    """
+     Access point to the database
+    """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    mycursor = db.cursor()
+    mycursor.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name LIKE BINARY %(name)s
+            ORDER BY
+                states.id ASC
+        """, {
+            'name': argv[4]
+        })
+    rows = mycursor.fetchall()
+
+    for row in rows:
+        print(row)
