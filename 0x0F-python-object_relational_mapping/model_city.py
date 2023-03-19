907ae8185fba7dbb7a9e2f30b6d5844c8f12d598
Martian66 <kimmartin049@gmail.com>
2023-03-19 03:57:14 -0700

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This script uses ORM to define a City class and
+a Base class that can be used to interact with a MySQL database """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class
+    Attributes:
+    __tablename__(str): the name of table class
+    id (int): The City's id
+    name (str): the City's name of the class
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
