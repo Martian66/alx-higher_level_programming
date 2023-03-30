a993c572b38e66d81e4a9f55b66cc155d4b1dcbc
Martian66 <kimmartin049@gmail.com>
2023-03-30 06:04:31 -0700

    test

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A bash script that takes in a URL and send a GET request to the URL
-curl -s -w '%{http_code}' "$1" | { read status; [ "$status" = "200" ] && curl -s "$1" echo "Error: Response status code is $status"; } | tail -n +2
+curl -sX GET $1 -L
