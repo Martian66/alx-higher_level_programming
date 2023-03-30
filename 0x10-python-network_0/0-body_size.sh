95ba9492b827e58c6ff8eb8834b5b30caab01669
Martian66 <kimmartin049@gmail.com>
2023-03-30 04:46:55 -0700

    produces a 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes a URL, sends a request, and displays the body size in bytes.
-curl -sI "$1" | grep Content-length | cut -d: -f2 | tr -d " "
+curl -sL "$1" | wc -c
