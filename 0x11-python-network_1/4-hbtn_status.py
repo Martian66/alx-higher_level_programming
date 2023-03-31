7024243e6f1a0fc7dee07e0502dff1a97ecadd3f
Martian66 <kimmartin049@gmail.com>
2023-03-31 12:58:16 -0700

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 response = requests.get('https://alx-intranet.hbtn.io/status')
 
 if response.status_code == 200:
-    content = response.json()
+    content = response.text
     print('Body response:')
     print('\t- type:', type(content))
     print('\t- content:', content)
