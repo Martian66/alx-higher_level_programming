2203581abccb76eefe27cc5c41ae7e0e57209821
Martian66 <kimmartin049@gmail.com>
2023-04-01 11:30:28 -0700

    checking

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# sends a POST request to http://0.0.0.0:5000/search_user
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+
+    response = requests.post(url, data={"q": q})
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
