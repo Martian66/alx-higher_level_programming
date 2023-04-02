d9648ad9349c74885b1e4338b065c0f57d5a1b3d
Martian66 <kimmartin049@gmail.com>
2023-04-02 06:21:00 -0700

    update

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,17 +5,11 @@ and uses the GitHub API to display your id
 """
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
-username = sys.argv[1]
-password = sys.argv[2]
 
-url = 'https://api.github.com/user'
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
 
-response = requests.get(url, auth=(Martian66, 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))
-
-if response.status_code == 200:
-    data = response.json()
-    user_id = data['id']
-    print(f"Your user ID is {user_id}")
-else:
-print("Failed to retrieve user ID. Please check your credentials and try again.")
+    print(req.json().get("id"))
