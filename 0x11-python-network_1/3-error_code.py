cea4331d9c1eacae7f913670e0826194046dbd1c
Martian66 <kimmartin049@gmail.com>
2023-04-02 08:40:28 -0700

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
 
     try:
         with urlopen(req) as response:
-            print(response.read().decode('utf-8'))
+            print(response.read().decode("ascii"))
     except HTTPError as e:
         print("Error code: {}".format(e.code))
